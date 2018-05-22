6180c214ae5b5e782bb28ef21603d9fc7fba356c
Karol Kromski <kromski.k@gmail.com>
2018-05-22 13:32:51 +0100

    testing option to create new account with valid informations

--- src/test/java/karolk/tests/automationPractiseCom/RegistrationFormTest.java
+++ src/test/java/karolk/tests/automationPractiseCom/RegistrationFormTest.java
@@ -1,4 +1,19 @@
 package karolk.tests.automationPractiseCom;
 
-public class RegistrationFormTest {
+import karolk.BaseTest;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationFormTest extends BaseTest {
+    @Test
+    public void CreateNewAccount() {
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElementByCssSelector(".login").click();
+        assertEquals("Authentication",
+                driver.findElementByCssSelector(".navigation_page").getText()
+        );
+        driver.findElementById("email_create").sendKeys("asda@gmail.com");
+        driver.findElementById("SubmitCreate").click();
+    }
 }
