5df24797e5fe57299e7e0f6c891e05d1f301f600
Karol Kromski <kromski.k@gmail.com>
2018-05-24 14:03:24 +0100

    extension of registration form test

--- src/test/java/karolk/tests/automationPractiseCom/RegistrationFormTest.java
+++ src/test/java/karolk/tests/automationPractiseCom/RegistrationFormTest.java
@@ -1,9 +1,13 @@
 package karolk.tests.automationPractiseCom;
 
+import javafx.scene.control.RadioButton;
 import karolk.BaseTest;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.awt.image.ByteInterleavedRaster;
 
 import java.awt.*;
 
@@ -20,16 +24,49 @@ public class RegistrationFormTest extends BaseTest {
                 driver.findElementByCssSelector(".navigation_page").getText()
         );
         // fill email field
-        driver.findElementById("email_create").sendKeys("deba@gmail.com");
+        driver.findElementById("email_create").sendKeys("debassss@gmail.com");
         driver.findElementById("SubmitCreate").click();
         //wait for form to appear
         wait.until(
                 ExpectedConditions.visibilityOfElementLocated(
-                    By.id("account-creation_form")
+                        By.id("account-creation_form")
                 )
         );
         //checked if button is clicked
-        driver.findElementById("id_gender2").click();
+        driver.findElementById("id_gender1").click();
+
+        //filling up form about Your Personal Details
+        driver.findElementByName("customer_firstname").sendKeys("lala");
+        driver.findElementByName("customer_lastname").sendKeys("kaka");
+        driver.findElementById("passwd").sendKeys("12345");
+        driver.findElementById("days").sendKeys("1");
+        driver.findElementById("months").sendKeys("January");
+        driver.findElementById("years").sendKeys("2010");
+        driver.findElementById("newsletter").click();
+        driver.findElementById("optin").click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.id("customer_firstname")
+                )
+        );
+        //filling up form about Adress Details
+        assertEquals("lala",
+                driver.findElementById("firstname").getAttribute("value"));
+        assertEquals("kaka",
+                driver.findElementById("lastname").getAttribute("value"));
+        driver.findElementById("company").sendKeys("bla");
+        driver.findElementById("address1").sendKeys("devon avenue");
+        driver.findElementById("address2");
+        driver.findElementById("city").sendKeys("Norwich");
+        driver.findElementById("postcode").sendKeys("00000");
+        driver.findElementById("id_state").sendKeys("Alabama");
+        driver.findElementById("other").sendKeys("wrrr");
+        driver.findElementById("phone").sendKeys("12345565");
+        driver.findElementById("phone_mobile").sendKeys("223345511");
+        driver.findElementById("submitAccount").click();
+        assertEquals("My account",
+                driver.findElementByClassName("navigation_page").getText());
+
 
     }
 
@@ -41,7 +78,7 @@ public class RegistrationFormTest extends BaseTest {
         assertEquals("Authentication",
                 driver.findElementByCssSelector(".navigation_page").getText()
         );
-        driver.findElementById("email_create").sendKeys("asda");
+        driver.findElementById("email_create").sendKeys("lala");
         driver.findElementById("SubmitCreate").click();
     }
 }
