51f820b58dc2d093bc6473e8f1bbcb710fce779b
Karol Kromski <kromski.k@gmail.com>
2018-06-08 12:49:18 +0100

    accomplished test

--- src/test/java/karolk/tests/automationPractiseCom/ContactFormTest.java
+++ src/test/java/karolk/tests/automationPractiseCom/ContactFormTest.java
@@ -3,25 +3,56 @@ package karolk.tests.automationPractiseCom;
 import karolk.BaseTest;
 import org.junit.Test;
 import org.openqa.selenium.By;
+
 import static org.junit.Assert.*;
 
 public class ContactFormTest extends BaseTest {
     @Test
     public void contactButton() {
-      driver.get("http://automationpractice.com/index.php");
-      driver.findElementById("contact-link").click();
-        assertEquals("Contact",
-                driver.findElementByClassName("navigation_page").getText()
-                );
-        driver.findElementById("id_contact").sendKeys("Customer Service");
-        driver.findElementById("email").sendKeys("asdas@gmail.com");
-        driver.findElementById("id_order").sendKeys("blebleble");
-        driver.findElementById("message").sendKeys("aaaaaaaaaaaaaaa");
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElementById("contact-link")
+                .click();
+        assertEquals(
+                "Contact",
+                driver.findElementByClassName("navigation_page")
+                        .getText()
+        );
+        driver.findElementById("id_contact")
+                .sendKeys("Customer Service");
+        driver.findElementById("email")
+                .sendKeys("asdas@gmail.com");
+        driver.findElementById("id_order")
+                .sendKeys("blebleble");
+        driver.findElementById("message")
+                .sendKeys("aaaaaaaaaaaaaaa");
+        driver.findElementById("submitMessage")
+                .click();
+        assertEquals(
+                "CUSTOMER SERVICE - CONTACT US",
+                driver.findElementByClassName("page-heading")
+                        .getText()
+
+        );
+    }
+
+    @Test
+    public void contactButtonInvalidDetails() {
+        driver.get("http://automationpractice.com/index.php");
+        driver.findElementById("contact-link")
+                .click();
+        assertEquals(
+                "Contact",
+                driver.findElementByClassName("navigation_page")
+                        .getText()
+        );
+        driver.findElementById("id_contact").sendKeys();
+        driver.findElementById("email").sendKeys("asdas");
+        driver.findElementById("id_order").sendKeys();
+        driver.findElementById("message").sendKeys();
         driver.findElementById("submitMessage").click();
-        assertEquals("CUSTOMER SERVICE - CONTACT US",
-                driver.findElementByClassName("page-heading").getText()
+        assertEquals("There is 1 error",
+                driver.findElementByCssSelector("div.alert-danger p").getText()
                 );
     }
-    
 
 }
